9b81c3bb878f22e063866f80d87d029893e49279
kkkivannn <ikiselev288@gmail.com>
2022-10-18 15:19:58 +0300

    Complete SignIn and SignUp

--- server/lib/controllers/app_auth_controller.dart
+++ server/lib/controllers/app_auth_controller.dart
@@ -1,5 +1,7 @@
+import 'package:jaguar_jwt/jaguar_jwt.dart';
 import 'package:server/models/response_model.dart';
 import 'package:server/server.dart';
+import 'package:server/utils/app_utils.dart';
 
 import '../models/user_data.dart';
 
@@ -14,17 +16,38 @@ class AppAuthController extends ResourceController {
         body: ResponseModel(message: "Поля Password и Username обязательны!"),
       );
     }
-    final User fetchedUser = User();
-    return Response.ok(
-      ResponseModel(
-        data: {
-          'id': fetchedUser.id,
-          'refreshToken': fetchedUser.refreshToken,
-          'accessToken': fetchedUser.accessToken,
-        },
-        message: 'Успешная авторизация!',
-      ).toJson(),
-    );
+    try {
+      final qFindUser = Query<User>(managedContext)
+        ..where((table) => table.username).equalTo(user.username)
+        ..returningProperties((table) => [
+              table.id,
+              table.salt,
+              table.hashPassword,
+            ]);
+      final findUser = await qFindUser.fetchOne();
+      if (findUser == null) {
+        throw QueryException.conflict("Пользователь не найден", []);
+      }
+      final requestHashPassword = AuthUtility.generatePasswordHash(
+          user.password ?? '', findUser.salt ?? '');
+      if (requestHashPassword == findUser.hashPassword) {
+        await _updateTokens(findUser.id ?? -1, managedContext);
+
+        final newUser =
+            await managedContext.fetchObjectWithID<User>(findUser.id);
+        return Response.ok(
+          ResponseModel(
+              data: newUser?.backing.contents,
+              message: "Успешная авторизация!"),
+        );
+      } else {
+        throw QueryException.conflict("Пароль не верный", []);
+      }
+    } on QueryException catch (error) {
+      return Response.serverError(
+        body: ResponseModel(message: error.message),
+      );
+    }
   }
 
   @Operation.put()
@@ -35,22 +58,53 @@ class AppAuthController extends ResourceController {
             message: "Поля Password и Username, и Email обязательны!"),
       );
     }
-    final User fetchedUser = User();
-    return Response.ok(
-      ResponseModel(
-        data: {
-          'id': fetchedUser.id,
-          'refreshToken': fetchedUser.refreshToken,
-          'accessToken': fetchedUser.accessToken,
-        },
-        message: 'Успешная регистрация!',
-      ).toJson(),
-    );
+    final salt = AuthUtility.generateRandomSalt();
+    final hashPassword =
+        AuthUtility.generatePasswordHash(user.password ?? "", salt);
+    try {
+      late final int id;
+      await managedContext.transaction((transaction) async {
+        final qCreateUser = Query<User>(transaction)
+          ..values.username = user.username
+          ..values.email = user.email
+          ..values.salt = salt
+          ..values.hashPassword = hashPassword;
+        final createdUser = await qCreateUser.insert();
+        id = createdUser.asMap()["id"] as int;
+        await _updateTokens(id, transaction);
+      });
+      final User? userData = await managedContext.fetchObjectWithID<User>(id);
+      return Response.ok(
+        ResponseModel(
+            data: userData?.backing.contents, message: "Успешная регистрация"),
+      );
+    } on QueryException catch (error) {
+      return Response.serverError(
+        body: ResponseModel(message: error.message),
+      );
+    }
+  }
+
+  Future<void> _updateTokens(int id, ManagedContext transaction) async {
+    final Map<String, dynamic> tokens = _getTokens(id);
+    final qUpdateTokens = Query<User>(transaction)
+      ..where((user) => user.id).equalTo(id)
+      ..values.accessToken = tokens["access"] as String
+      ..values.refreshToken = tokens["refresh"] as String;
+    await qUpdateTokens.updateOne();
   }
 
   @Operation.post('refresh')
   Future<Response> refreshToken(
       @Bind.path('refresh') String refreshToken) async {
+    try {
+      final id = AppUtils.getIdFromToken(refreshToken);
+    } catch (error) {
+      return Response.serverError(
+        body: ResponseModel(message: error.toString()),
+      );
+    }
+
     final User fetchedUser = User();
     return Response.ok(
       ResponseModel(
@@ -62,5 +116,21 @@ class AppAuthController extends ResourceController {
         message: 'Успешное обновление!',
       ).toJson(),
     );
+  }
+
+  Map<String, dynamic> _getTokens(id) {
+    //TODO remove when release
+    final key = Platform.environment['SECRET_KEY'] ?? 'SECRET_KEY';
+    final accessClaimSet = JwtClaim(
+      maxAge: const Duration(hours: 1),
+      otherClaims: {"id": id},
+    );
+    final refreshClaimSet = JwtClaim(
+      otherClaims: {"id": id},
+    );
+    final tokens = <String, dynamic>{};
+    tokens["access"] = issueJwtHS256(accessClaimSet, key);
+    tokens["refresh"] = issueJwtHS256(refreshClaimSet, key);
+    return tokens;
   }
 }
--- server/lib/models/user_data.dart
+++ server/lib/models/user_data.dart
@@ -1,4 +1,4 @@
-import 'package:server/server.dart';
+import 'package:conduit/conduit.dart';
 
 class User extends ManagedObject<_User> implements _User {}
 
@@ -9,9 +9,14 @@ class _User {
   String? username;
   @Column(unique: true, indexed: true)
   String? email;
+  @Serialize(input: true, output: false)
   String? password;
   @Column(nullable: true)
   String? accessToken;
   @Column(nullable: true)
   String? refreshToken;
+  @Column(omitByDefault: true)
+  String? salt;
+  @Column(omitByDefault: true)
+  String? hashPassword;
 }
